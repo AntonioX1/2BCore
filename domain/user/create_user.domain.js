65108a36e91f8395a8207dde147e6db82f6af295
Antonio Cortés <antonio.cortes.1901@gmail.com>
2021-10-20 19:59:39 -0500

    Fix/Solución a encriptado de password

--- domain/user/create_user.domain.js
+++ domain/user/create_user.domain.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const { genSaltSync, hashSync, genSalt } = require('bcrypt');
+const { genSaltSync, hashSync } = require('bcrypt');
 
 const createUserDomain = async (email = '', password = '') => {
 
 	const user = {
 		email,
-		password: hashSync(password, genSalt(10))
+		password: hashSync(password, genSaltSync(10))
 	};
 
 	return user;

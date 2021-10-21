a71b434ad45d742efd8fcce9fa17ef1af71a78b4
Antonio Cortés <antonio.cortes.1901@gmail.com>
2021-10-20 23:52:24 -0500

    Fix/Actualización del archivo bin

--- bin/www.js
+++ bin/www.js
@@ -3,10 +3,5 @@
 require('dotenv').config();
 
 const app = require('../app');
-const PgConnection = require('../db/pg.connection');
 
-app.listen(process.env._PORT, () => {
-
-  console.log(`Server listen to: http://localhost:${ process.env._PORT }`)
-
-});
+app.listen(process.env._PORT, () => console.log(`Server listen to: http://localhost:${ process.env._PORT }`));

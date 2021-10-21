032e74779f606fdd1bd7b3c9d5097340ee611239
Antonio Cortés <antonio.cortes.1901@gmail.com>
2021-10-20 23:51:41 -0500

    Fix/Actualización del router principal

--- router.js
+++ router.js
@@ -4,4 +4,6 @@ const router = require('express').Router();
 
 router.use('/user', require('./routes/users/user.router'));
 
+router.use('/role', require('./routes/role/role.router'));
+
 module.exports = router;

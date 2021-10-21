e97143a2b0cc0c4ae5a211f37c31115335f532f8
Antonio Cortés <antonio.cortes.1901@gmail.com>
2021-10-20 19:35:15 -0500

    Fix/Solución de problema con conexión a la base de datos

--- db/pg.connection.js
+++ db/pg.connection.js
@@ -6,7 +6,6 @@ class PgConnection {
 
 	constructor() {
 
-
 		this._pool = new Pool({
 			user: process.env._PGUSER,
 			host: process.env._PGHOST,
@@ -14,14 +13,6 @@ class PgConnection {
 			password: process.env._PGPASSWORD,
 			port: process.env._PGPORT,
 		});
-
-		this._pool.connect((error, client) => console.log(error));
-
-	}
-
-	async query(SQLString = '', SQLValues = []) {
-
-		return await this._pool.query(SQLValues, SQLString);
 
 	}
 
